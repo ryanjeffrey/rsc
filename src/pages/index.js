dd552855ee813d27a2ffba4d61e814ecbeee8189
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-05-21 11:49:07 -0500

    Installed gatsby-plugin-image and added img to folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import Navbar from "../components/Navbar"
+import "../assets/css/main.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+const index = () => {
+  return (
+      <div>
+        <Navbar />
+        <div className="hero-section">
+          <h1>Beautiful design.</h1>
+          <h2>Eloquent development.</h2>
+          <h2>Dope websites.</h2>
+        </div>
+      </div>
+  )
 }
+
+export default index
