bced1fa381991c9b481805294c215a672901cc7c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-05-26 12:08:18 -0500

    Added css transition to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero = () => {
     return (
       <div className="hero-section">
-        <h1>Beautiful design.</h1>
-        <h2>Eloquent development.</h2>
-        <h2>Dope websites.</h2>
+        <h1 className="heading-1-large load-1st">Beautiful design.</h1>
+        <h2 className="heading-1-large load-2nd">Eloquent development.</h2>
+        <h2 className="heading-1-large load-3rd">Dope websites.</h2>
       </div>
     )
 }
