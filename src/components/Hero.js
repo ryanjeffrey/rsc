63d09c34184097676df63476675bb18cf794f308
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-05-27 16:51:55 -0500

    Added hero with mobile stack

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
     return (
       <div className="hero-section">
+        <StaticImage
+          src="../assets/images/stack.jpg"
+          alt=""
+          style={{ zIndex: -1 }}
+          placeholder="blurred"
+        />
         <h1 className="heading-1-large load-1st">Beautiful design.</h1>
         <h2 className="heading-1-large load-2nd">Eloquent development.</h2>
         <h2 className="heading-1-large load-3rd">Dope websites.</h2>
