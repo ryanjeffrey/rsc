5cddf5027651e1d6da60e153e359afefd350f975
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-06-23 15:18:24 -0500

    Padding

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,14 +7,16 @@ class Hero extends Component {
     return (
       <Transition timeout={4000} in={true} appear>
         {status => (
-          <div className={`box box-${status}`}>
-            <h1 className="heading-1-large load-1st">We make websites that make you more money.</h1>
-            <StaticImage
+          <div className="hero-section">
+            <div className={`box box-${status}`}>
+              <h1 className="heading-1-large load-1st">We make websites that make you more money.</h1>
+              <StaticImage
               src="../assets/images/stack.jpg"
               alt=""
               style={{ zIndex: -1 }}
               placeholder="blurred"
-            />
+              />
+            </div>
           </div>
         )}
       </Transition>
